6ddb1a11384cc9785865d7c2277f5df12620a220
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-08-07 20:34:53 +0700

    Get Form Data in Data Property. Call function on login button. Test API for Login. Call API for Login. Redirect on Login API success.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,7 @@
     <div class="login">
         <input type="text" v-model="emailInput" placeholder="Enter Email"/>
         <input type="password" v-model="passwordInput" placeholder="Enter Password"/>
-        <button>Login</button>
+        <button @click="login()">Login</button>
         <p>
             <router-link to="/sign-up">Sign Up</router-link>
         </p>
@@ -12,7 +12,36 @@
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default{
         name: 'LoginPage',
+        data(){
+            return{
+                emailInput:'',
+                passwordInput:''
+            }
+        },
+        methods:{
+            login: async function(){
+                try{
+                    var user = await (await axios.get(`http://localhost:3001/users?email=${this.emailInput}&password=${this.passwordInput}`)).data;
+                    if(user != null){
+                        localStorage.setItem('user-info', JSON.stringify(user));
+                        this.$router.push({name:'Home'});
+                    }
+                }
+                catch(e){
+                    console.log(e);
+                }
+            }
+        },
+        async mounted(){
+            // Check if user login, return homepage but not sign up. 
+            let user = await localStorage.getItem('user-info');
+            if(user != null){
+                this.$router.push({name:'Home'});
+            }
+        }
     }
 </script>
