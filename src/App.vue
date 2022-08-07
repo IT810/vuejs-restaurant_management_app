845c7064809f22a52f3ed58d033145bc6b7b6b66
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-08-07 17:50:00 +0700

    Make Component for Sign-Up. Add SignUp Component in App Component. Add Input Fields. Add Style to Component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignUp></SignUp>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import SignUp from './components/SignUp.vue'
+  export default {
+    name: 'App',
+    components:{
+      SignUp
+    }
   }
-}
 </script>
 
 <style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,42 @@
+<template>
+    <img class="logo" src="../assets/logo.png">
+    <h1>Sign Up</h1>
+    <div class="register">
+        <input type="text" placeholder="Enter Name"/>
+        <input type="text" placeholder="Enter Email"/>
+        <input type="password" placeholder="Enter Password"/>
+        <button>Sign Up</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'SignUp'
+    }
+</script>
+
+<style scoped>
+    .logo{
+        width: 100px;
+    }
+
+    .register input{
+        width: 300px;
+        height: 40px;
+        padding-left: 40px;
+        display: block;
+        margin-bottom: 30px;
+        margin-right: auto;
+        margin-left: auto;
+        border: 1px solid blue;
+    }
+
+    .register button{
+        width: 350px;
+        height: 40px;
+        border: 1px solid skyblue;
+        background: skyblue;
+        color: #fff;
+        cursor: pointer;
+    }
+</style>
