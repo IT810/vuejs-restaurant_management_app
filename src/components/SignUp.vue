5ed1f550a094d2d20f3e464a657d7db889f325c8
Hung Nguyen <hung.187pm20486@vanlanguni.vn>
2022-08-07 18:44:43 +0700

    Make API for SignUP User. Get Value from Input fields. Call function on Button Click. Install Axios Package for API. Call API. Store Data in localStorage.

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,16 +2,41 @@
     <img class="logo" src="../assets/logo.png">
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter Name"/>
-        <input type="text" placeholder="Enter Email"/>
-        <input type="password" placeholder="Enter Password"/>
-        <button>Sign Up</button>
+        <input type="text" v-model="nameInput" placeholder="Enter Name"/>
+        <input type="text" v-model="emailInput" placeholder="Enter Email"/>
+        <input type="password" v-model="passwordInput" placeholder="Enter Password"/>
+        <button @click="signUp()">Sign Up</button>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default{
-        name:'SignUp'
+        name:'SignUp',
+        data(){
+            return{
+                nameInput:'',
+                emailInput:'',
+                passwordInput:''
+            }
+        },
+        methods:{
+            signUp: async function(){
+                try{
+                    var res = await axios.post('http://localhost:3001/users', { name: this.nameInput, email: this.emailInput, password: this.passwordInput});
+                    console.log(res);
+                    alert("Sign up successfully !!!");
+                    this.nameInput = '';
+                    this.emailInput = '';
+                    this.passwordInput = '';
+                }
+                catch(ex){
+                    console.log(ex);
+                    localStorage.setItem("user-info", JSON.stringify(res.data));
+                }
+            }
+        }
     }
 </script>
 
