10ce0c51ad863dde74b121a0a790ea9490dc8f70
Hung Nguyen <hung.nguyen@nois.vn>
2022-08-08 10:29:05 +0700

    Install Axios Package for API. Test API with the postman. Call API. Make List from API data.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,55 @@
 <template>
     <Header></Header>
-    <h1>Hello {{name}}, Welcome to HomePage.</h1>
+    <h1>Hello {{userName}}, Welcome to HomePage.</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Contact</th>
+                <th>Address</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in restaurants" :key="item.id">
+                <td>{{item.id}}</td>
+                <td>{{item.name}}</td>
+                <td>{{item.contact}}</td>
+                <td>{{item.address}}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
 import Header from './Header.vue'
-
+import axios from 'axios'
 export default {
     name: "HomePage",
     data(){
         return{
-            name:'',
+            userName:'',
+            restaurants:[]
         }
     },
     async mounted() {
         // Check if user do not login, return sign up but not homepage. 
-        let user = await localStorage.getItem("user-info");
-        this.name = JSON.parse(user).name;
+        let user = await localStorage.getItem('user-info');
+        this.userName = JSON.parse(user).name;
         if (user == null) {
             this.$router.push({ name: "Login" });
         }
+
+        let result = await axios.get('http://localhost:3001/restaurants');
+        this.restaurants = result.data;
     },
     components: { Header }
 }
 </script>
 
 <style scoped>
+td{
+    width: 160px;
+    height: 40px;
+}
 </style>
